7a31ac32edd146a9f08fc4e7439db325eb3bf736
Falokut <71385842+Falokut@users.noreply.github.com>
2023-11-24 19:28:21 +0300

    creating golang grpc-rest-server module

---
+++ go.mod
@@ -0,0 +1,34 @@
+module github.com/Falokut/grpc_rest_server
+
+go 1.21.4
+
+require (
+	github.com/Falokut/interceptors v1.0.1
+	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0
+	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.18.1
+	github.com/grpc-ecosystem/grpc-opentracing v0.0.0-20180507213350-8e809c8a8645
+	github.com/opentracing/opentracing-go v1.2.0
+	github.com/sirupsen/logrus v1.9.3
+	github.com/soheilhy/cmux v0.1.5
+	google.golang.org/grpc v1.59.0
+)
+
+require (
+	github.com/Falokut/grpc_errors v1.0.0 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/prometheus/client_golang v1.17.0 // indirect
+	github.com/prometheus/client_model v0.4.1-0.20230718164431-9a2bf3000d16 // indirect
+	github.com/prometheus/common v0.44.0 // indirect
+	github.com/prometheus/procfs v0.11.1 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20231106174013-bbf56f31fb17 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231030173426-d783a09b4405 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
---
+++ server.go
@@ -0,0 +1,152 @@
+package grpc_rest_server
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+	"strings"
+
+	"github.com/Falokut/interceptors"
+	"github.com/opentracing/opentracing-go"
+	"github.com/sirupsen/logrus"
+	"github.com/soheilhy/cmux"
+
+	grpcrecovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
+	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
+	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/grpc-ecosystem/grpc-opentracing/go/otgrpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type server struct {
+	service        any
+	logger         *logrus.Logger
+	grpcServer     *grpc.Server
+	AllowedHeaders []string
+	im             *interceptors.InterceptorManager
+	mux            cmux.CMux
+}
+
+func NewServer(logger *logrus.Logger, service any) server {
+	return server{logger: logger, service: service}
+}
+
+type Config struct {
+	Host           string
+	Port           string
+	Mode           string
+	AllowedHeaders []string
+
+	ServiceDesc               *grpc.ServiceDesc
+	RegisterRestHandlerServer func(ctx context.Context, mux *runtime.ServeMux, service any) error
+}
+
+func (s *server) Run(cfg Config, metric interceptors.Metrics, grpcOptions *[]grpc.ServerOption, customRestMux *runtime.ServeMux) {
+	Mode := strings.ToUpper(cfg.Mode)
+	s.logger.Info("start running server on mode: " + Mode)
+	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%s", cfg.Host, cfg.Port))
+	if err != nil {
+		s.logger.Fatal("error while listening", err)
+	}
+	s.mux = cmux.New(lis)
+	s.im = interceptors.NewInterceptorManager(s.logger, metric)
+	s.AllowedHeaders = cfg.AllowedHeaders
+
+	switch Mode {
+	case "REST":
+		s.runRestAPI(cfg, customRestMux)
+	case "GRPC":
+		s.runGRPC(cfg, grpcOptions)
+	case "BOTH":
+		s.runRestAPI(cfg, customRestMux)
+		s.runGRPC(cfg, grpcOptions)
+	}
+	if err := s.mux.Serve(); err != nil {
+		s.logger.Fatal(err)
+	}
+	grpc_prometheus.Register(s.grpcServer)
+
+	s.logger.Info("server running on mode: " + Mode)
+}
+
+func (s *server) runGRPC(cfg Config, serverOptions *[]grpc.ServerOption) {
+	s.logger.Info("GRPC server initializing")
+
+	if serverOptions == nil || len(*serverOptions) < 1 {
+		serverOptions = &[]grpc.ServerOption{grpc.UnaryInterceptor(s.im.Logger), grpc.ChainUnaryInterceptor(
+			otgrpc.OpenTracingServerInterceptor(opentracing.GlobalTracer()),
+			grpc_ctxtags.UnaryServerInterceptor(),
+			s.im.Metrics,
+			grpcrecovery.UnaryServerInterceptor(),
+		), grpc.StreamInterceptor(s.im.StreamLogger), grpc.ChainStreamInterceptor(
+			otgrpc.OpenTracingStreamServerInterceptor(opentracing.GlobalTracer()),
+			grpc_ctxtags.StreamServerInterceptor(),
+			s.im.StreamMetrics,
+			grpcrecovery.StreamServerInterceptor(),
+		), grpc.Creds(insecure.NewCredentials()),
+		}
+	}
+
+	s.grpcServer = grpc.NewServer(*serverOptions...)
+
+	s.grpcServer.RegisterService(cfg.ServiceDesc, s.service)
+	go func() {
+		grpcL := s.mux.Match(cmux.HTTP2HeaderField("content-type", "application/grpc"))
+		if err := s.grpcServer.Serve(grpcL); err != nil {
+			s.logger.Fatalf("GRPC error while serving: %v", err)
+		}
+	}()
+	s.logger.Infof("GRPC server initialized. Listen on %s:%s", cfg.Host, cfg.Port)
+}
+
+func (s *server) runRestAPI(cfg Config, mux *runtime.ServeMux) {
+	s.logger.Info("REST server initializing")
+
+	if mux == nil {
+		mux = runtime.NewServeMux(
+			runtime.WithIncomingHeaderMatcher(s.headerMatcherFunc),
+		)
+	}
+
+	if err := cfg.RegisterRestHandlerServer(context.Background(), mux, s.service); err != nil {
+		s.logger.Fatalf("REST server error while registering handler server: %v", err)
+
+	}
+
+	server := http.Server{
+		Handler: s.im.RestLogger(s.im.RestMetrics(mux)),
+	}
+
+	s.logger.Info("Rest server initializing")
+	go func() {
+		restL := s.mux.Match(cmux.HTTP1Fast())
+
+		if err := server.Serve(restL); err != nil {
+			s.logger.Fatalf("REST server error while serving: %v", err)
+		}
+	}()
+
+	s.logger.Infof("REST server initialized. Listen on %s:%s", cfg.Host, cfg.Port)
+}
+
+func (s *server) Shutdown() {
+	s.logger.Println("Shutting down")
+	if s.grpcServer != nil {
+		s.grpcServer.GracefulStop()
+	}
+	s.mux.Close()
+}
+
+func (s *server) headerMatcherFunc(header string) (string, bool) {
+	s.logger.Debugf("Received %s header", header)
+	for _, AllowedHeader := range s.AllowedHeaders {
+		if header == AllowedHeader {
+			return AllowedHeader, true
+		}
+	}
+
+	return runtime.DefaultHeaderMatcher(header)
+}
